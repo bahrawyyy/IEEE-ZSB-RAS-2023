758792062e6c217caa3d00efb5dd92988c072f70
Abdalla Elbahrawy <abdallabahrawyy@gmail.com>
2023-07-23 23:55:52 +0300

    Security (DC & Stepper)

---
+++ Task14/Simple_Security_System/Debug/APP/main.d
@@ -0,0 +1,42 @@
+APP/main.o APP/main.o: ../APP/main.c ../APP/../HAL/Lcd/Lcd.h \
+  ../APP/../HAL/Lcd/Lcd_Config.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/Dio_interface.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/STD_Types.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/ErrorState.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/Bit_Math.h \
+  ../APP/../HAL/KPD/KPD_interface.h \
+  ../APP/../HAL/KPD/../../LIB/ErrorState.h \
+  ../APP/../HAL/KPD/../../LIB/STD_Types.h \
+  ../APP/../HAL/DC_Motor/DC_Motor.h \
+  ../APP/../HAL/DC_Motor/../../MCAL/Dio/Dio_interface.h \
+  ../APP/../HAL/DC_Motor/../../MCAL/Registers.h \
+  ../APP/../HAL/Stepper_Motor/Stepper.h \
+  ../APP/../HAL/Stepper_Motor/../../MCAL/Dio/Dio_interface.h
+
+../APP/../HAL/Lcd/Lcd.h:
+
+../APP/../HAL/Lcd/Lcd_Config.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/Dio_interface.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/STD_Types.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/ErrorState.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/Bit_Math.h:
+
+../APP/../HAL/KPD/KPD_interface.h:
+
+../APP/../HAL/KPD/../../LIB/ErrorState.h:
+
+../APP/../HAL/KPD/../../LIB/STD_Types.h:
+
+../APP/../HAL/DC_Motor/DC_Motor.h:
+
+../APP/../HAL/DC_Motor/../../MCAL/Dio/Dio_interface.h:
+
+../APP/../HAL/DC_Motor/../../MCAL/Registers.h:
+
+../APP/../HAL/Stepper_Motor/Stepper.h:
+
+../APP/../HAL/Stepper_Motor/../../MCAL/Dio/Dio_interface.h:

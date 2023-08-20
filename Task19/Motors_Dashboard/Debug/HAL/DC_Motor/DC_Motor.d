b3bb303b9487b94bfcf74b5296ff19f9c03d111d
Abdalla Elbahrawy <abdallabahrawyy@gmail.com>
2023-08-20 19:14:24 +0300

    Task19 : Motors Dashboard

---
+++ Task19/Motors_Dashboard/Debug/HAL/DC_Motor/DC_Motor.d
@@ -0,0 +1,21 @@
+HAL/DC_Motor/DC_Motor.o HAL/DC_Motor/DC_Motor.o:  \
+ ../HAL/DC_Motor/DC_Motor.c ../HAL/DC_Motor/DC_Motor.h \
+  ../HAL/DC_Motor/../../MCAL/Dio/Dio_interface.h \
+  ../HAL/DC_Motor/../../MCAL/Dio/../../LIB/STD_Types.h \
+  ../HAL/DC_Motor/../../MCAL/Dio/../../LIB/ErrorState.h \
+  ../HAL/DC_Motor/../../MCAL/Dio/../../LIB/Bit_Math.h \
+  ../HAL/DC_Motor/../../MCAL/Registers.h ../HAL/DC_Motor/DC_MotorCfg.h
+
+../HAL/DC_Motor/DC_Motor.h:
+
+../HAL/DC_Motor/../../MCAL/Dio/Dio_interface.h:
+
+../HAL/DC_Motor/../../MCAL/Dio/../../LIB/STD_Types.h:
+
+../HAL/DC_Motor/../../MCAL/Dio/../../LIB/ErrorState.h:
+
+../HAL/DC_Motor/../../MCAL/Dio/../../LIB/Bit_Math.h:
+
+../HAL/DC_Motor/../../MCAL/Registers.h:
+
+../HAL/DC_Motor/DC_MotorCfg.h:
